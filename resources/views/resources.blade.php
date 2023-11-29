c714ffb40a58c588272dac3098a0810bc3118b04
AlunBowen <alunbowen1987@gmail.com>
2023-11-29 21:04:32 +0000

    Add route and view

---
+++ resources/views/resources.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Resources</h1>
+
+
+    @php
+        $assessments = App\Models\Assessment::all();
+    @endphp
+
+    @foreach($assessments as $assessment)
+        <p>{{ $assessment->name }}</p>
+        <p>{{ $assessment->description }}</p>
+    @endforeach
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+    </ul>
+
+
+</body>
+</html>
+
