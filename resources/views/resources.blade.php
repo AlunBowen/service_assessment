26fbf9ed607547003e8b2e92be9567401b805000
AlunBowen <alunbowen1987@gmail.com>
2023-12-04 08:45:34 +0000

    Update resource page grid

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -13,21 +13,22 @@
     @php
         $assessments = App\Models\Assessment::all();
     @endphp
-
-    @foreach($assessments as $assessment)
-
-    //card component
-    <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
-    <div class="card-body">
-        <p>{{ $assessment->name_en }}</p>
-        <p>{{ $assessment->description_en }}</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container">
+        <div class="row">
+            @foreach($assessments as $assessment)
+                <div class="col-md-4 mb-4">
+                    <div class="card">
+                        <img src="https://www.ringcentral.com/us/en/blog/wp-content/uploads/2020/07/Effective-teamwork.png.webp" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p>{{ $assessment->name_en }}</p>
+                            <p>{{ $assessment->description_en }}</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
-    </div>
-
-
-    @endforeach
     <ul>
         <li>1</li>
         <li>2</li>
